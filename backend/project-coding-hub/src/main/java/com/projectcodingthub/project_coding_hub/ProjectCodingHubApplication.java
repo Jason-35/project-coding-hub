8b5eba38ca0d1a43dac311bebbb87451e63c7ce0
Jason <jsonwu34@gmail.com>
2024-11-08 13:55:27 -0500

    chore: initalize project

---
+++ backend/project-coding-hub/src/main/java/com/projectcodingthub/project_coding_hub/ProjectCodingHubApplication.java
@@ -0,0 +1,13 @@
+package com.projectcodingthub.project_coding_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCodingHubApplication.class, args);
+	}
+
+}
---
+++ backend/project-coding-hub/src/test/java/com/projectcodingthub/project_coding_hub/ProjectCodingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectcodingthub.project_coding_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
