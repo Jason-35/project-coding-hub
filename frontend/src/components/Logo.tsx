d1ec0d385300a551d53416d436ce551fe67dfde7
Jason <jsonwu34@gmail.com>
2024-11-10 23:11:44 -0500

    style: styled login page

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <div className="text-[36px] font-semibold  hover:cursor-pointer"><span className="text-orange-400">Code</span> <span className="text-orange-400">Project</span> <span className="">Hub</span></div>
+    <div className="text-[36px] font-semibold  hover:cursor-pointer"><span className="text-orange-400 mr-[-0.4em]">Code</span> <span className="text-orange-400">Project</span> <span className="ml-[-0.4em]">Hub</span></div>
   )
 }
 
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,73 @@
+import { Form, useForm } from "react-hook-form"
+
+import { Lock, User, Icon } from "lucide-react"
+
+import Logo from "../components/Logo"
+import Coding from "../assets/coding.svg"
+import { Link } from "react-router-dom"
 function LoginPage() {
+  const { control } = useForm()
+
   return (
-    <div>LoginPage</div>
+    <div className="w-screen h-screen py-4 px-2 flex flex-col shadow-lg">
+      <div className="self-start ml-4">
+        <Logo />
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+          <Form 
+            className="flex flex-col bg-zinc-100 h-3/4 rounded-md p-12 gap-6 w-1/3 shadow-2xl"
+            action="/"
+            control={control}
+            onSuccess={() => {
+              alert("success")
+            }}
+            onError={() => {
+              alert("fail")
+            }}>
+            <div className="bg-orange-400 w-2/5 rounded-full aspect-square flex self-center">
+              <img src={Coding} alt="" />
+            </div>
+            <div className="self-center">
+              <span>Sign in to your account</span>
+            </div>
+            <div className="flex self-center w-4/5 focus-within:border-orange-400 border-4 rounded-md border-transparent">
+              <div className="p-2 flex justify-center items-center bg-black">
+                <User color="white" size={28}/>
+              </div>
+              <input 
+                className="self-center border-2 text-[16px] p-1 pl-3 w-full h-full border-none outline-none" 
+                type="text" 
+                placeholder="User name" />
+            </div>
+            <div className="self-center flex w-4/5 border-4 rounded-md focus-within:border-orange-400 border-transparent">
+              <div className="p-2 flex justify-center items-center bg-black">
+                <Lock color="white" size={28}/>
+              </div>
+              <input 
+                className="self-center text-[16px] p-1 pl-3 w-full h-full outline-none" 
+                type="text" 
+                placeholder="Password" />
+            </div>
+            <div className="self-center text-sm flex w-4/5 justify-between">
+              <div className="flex w-2/4 gap-2">
+                <input
+                  className="w-[24px]"
+                  type="checkbox" />
+                <span className="[word-spacing:-0.25em]  white">Remember me</span>
+              </div>
+              <div className="self-center">
+                <span className="[word-spacing:-0.25em] hover:underline hover:cursor-pointer">Forgot Password?</span>
+              </div>
+            </div>
+            <button className="border-2 self-center w-4/5 p-2 rounded-md border-black hover:bg-orange-400 hover:text-white hover:border-orange-500">
+              Sign In
+            </button>
+            <div className="self-center">
+              Not a member? <Link to={"/signup"} className="hover:underline">Create new account</Link>
+            </div>
+          </Form>
+      </div>
+    </div>
   )
 }
 
