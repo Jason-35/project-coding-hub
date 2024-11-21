2d75d8e21d17144487d0e2d58ea664f27e472046
Jason <jsonwu34@gmail.com>
2024-11-21 17:24:28 -0500

    style: added join button to post

--- frontend/src/features/projectServer/component/Post.tsx
+++ frontend/src/features/projectServer/component/Post.tsx
@@ -11,26 +11,38 @@ function Post({title, genre, description, icon, open} : PostProps) {
   return (
     <div className="w-96 rounded-lg border-2 p-2 h-max-92 scrollbar-hidden">
         <div className="flex border-b-4 border-orange-400 pb-1 bg-white rounded-t-full mb-2">
-            {icon ? <img width={64} height={64} src={icon} className="rounded-lg" /> : 
+            {icon ? <img width={64} height={64} src={icon} className="rounded-lg aspect-square" /> : 
             <div className="border-4 w-1/6 aspect-square rounded-lg flex justify-center items-center text-[32px]">
                 {title.charAt(0)}
             </div>}
-            <div className="flex flex-col justify-evenly px-2">
-                <div className="font-bold truncate w-72">{title}</div>
-                <div className="gap-1">
-                    <div className="">
-                        {open ? 
-                        <span className="border-2 border-black px-2 bg-green-400">
-                            public
-                        </span> :
-                        <span className="border-2 border-black px-2 bg-red-400">
-                            private
-                        </span>}
+            <div className="flex flex-1 px-2 justify-between">
+                <div>
+                    <div className="font-bold truncate w-52 ">{title}</div>
+                    <div className="gap-1">
+                        <div>
+                            {open ? 
+                            <span className="border-2 border-black px-2 bg-green-400">
+                                public
+                            </span> :
+                            <span className="border-2 border-black px-2 bg-red-400">
+                                private
+                            </span>}
+                        </div>
+                        <div className="">
+                            <span className="border-2 border-r-0 border-black px-1">1</span>
+                            <span className="border-2 border-black px-2">member</span>
+                        </div>
                     </div>
-                    <div className="">
-                        <span className="border-2 border-r-0 border-black px-1">1</span>
-                        <span className="border-2 border-black px-2">member</span>
-                    </div>
+                </div>
+                <div className="flex justify-center items-center">
+                    {open ? 
+                    <div className="hover:cursor-pointer hover:bg-blue-500 py-1 px-2 rounded-md bg-blue-400 text-white">
+                        Join
+                    </div> 
+                    : 
+                    <div className="hover:cursor-pointer bg-blue-400 text-white py-1 px-2 rounded-md hover:bg-blue-500">
+                        Request
+                    </div>}
                 </div>
                 
             </div>
--- frontend/src/pages/ProjectBoard.tsx
+++ frontend/src/pages/ProjectBoard.tsx
@@ -1,5 +1,6 @@
 import Post from "../features/projectServer/component/Post"
-import kitty from "../assets/flappy.png"
+import flappy from "../assets/flappy.png"
+import kitty from "../assets/kitty.png"
 
 function ProjectBoard() {
     let desc = "Creating spotify clone lf people who knows how to code the backend and ux designer for frontend. prefer beginner level coders. Experts are welcome to join. Mentors are welcome to help. Looking for 4 more members to start off this project. This is an open group for anyone"
@@ -13,7 +14,8 @@ function ProjectBoard() {
             </div>
         </div>
         <div className="p-10 flex flex-wrap gap-16">
-            <Post title="Spotify" description={desc} icon={kitty} open={true} genre={genre}/>
+            <Post title="Spotify" description={desc} icon={flappy} open={true} genre={genre}/>
+            <Post title="Jump King" description={desc} icon={kitty} open={false} genre={genre}/>
         </div>
     </div>
   )
