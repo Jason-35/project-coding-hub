ad33a3bff2ab864dc1684ed7ce62069096f98e68
Jason <jsonwu34@gmail.com>
2024-11-22 20:31:19 -0500

    feat: users name to user menu

--- frontend/src/features/user/component/UserMenu.tsx
+++ frontend/src/features/user/component/UserMenu.tsx
@@ -3,6 +3,9 @@ import { UserIcon, MailIcon, UsersIcon, LogOutIcon, SquarePlusIcon } from "lucid
 function UserMenu({showMenu} : {showMenu: boolean}) {
   return (
     <div className={`fixed ${showMenu ? "scale-100" : "scale-0"} rounded-lg transition duration-150 ease-in origin-top-left flex flex-col translate-x-24 w-52 h-fit bg-orange-400 gap-4 p-2`}>
+        <div className="pl-4 truncate">
+            Mango
+        </div>
         <div className="flex gap-2 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
             <div className="pl-1">ID:</div>
             <div>0198362465</div>
