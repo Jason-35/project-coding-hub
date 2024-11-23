44ce0108ae4b6e87fd30ac6a9acd502a9ac94658
Jason <jsonwu34@gmail.com>
2024-11-22 19:08:12 -0500

    style: user menu hover selection

--- frontend/src/features/user/component/UserMenu.tsx
+++ frontend/src/features/user/component/UserMenu.tsx
@@ -1,25 +1,29 @@
-import { UserIcon, MailIcon, UsersIcon, LogOutIcon } from "lucide-react"
+import { UserIcon, MailIcon, UsersIcon, LogOutIcon, SquarePlusIcon } from "lucide-react"
 
 function UserMenu({showMenu} : {showMenu: boolean}) {
   return (
-    <div className={`fixed ${showMenu ? "scale-100" : "scale-0"} transition duration-150 ease-in origin-top-left flex flex-col translate-x-24 w-52 h-fit bg-orange-400 gap-4 p-2`}>
-        <div className="flex">
-            <div className="px-1 pr-2">ID:</div>
+    <div className={`fixed ${showMenu ? "scale-100" : "scale-0"} rounded-lg transition duration-150 ease-in origin-top-left flex flex-col translate-x-24 w-52 h-fit bg-orange-400 gap-4 p-2`}>
+        <div className="flex gap-2 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
+            <div className="pl-1">ID:</div>
             <div>0198362465</div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
             <UserIcon />
             <div>Profile</div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
             <MailIcon />
             <div>Mail</div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
             <UsersIcon />
             <div>Friends</div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
+            <SquarePlusIcon />
+            <div>Create Project</div>
+        </div>
+        <div className="flex gap-4 hover:cursor-pointer hover:bg-black hover:text-white hover:rounded-md p-2">
             <LogOutIcon />
             <div>Logout</div>
         </div>
