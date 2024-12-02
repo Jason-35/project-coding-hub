75cde4c1df4fac8ef4a7b0fad010b4f123faf26e
Jason <jsonwu34@gmail.com>
2024-12-01 20:09:45 -0500

    style: create server form style done

--- frontend/src/features/server/component/CreateServer.tsx
+++ frontend/src/features/server/component/CreateServer.tsx
@@ -1,17 +1,19 @@
-import { PlusIcon, XIcon, CameraIcon } from "lucide-react"
+import { XIcon, CameraIcon } from "lucide-react"
 import { useRef, useState } from "react"
 import Chip from "../../../components/Chip"
+import { removeValue } from "../util/Util"
 
 function CreateServer({setShowCreateServer} : {setShowCreateServer:React.Dispatch<React.SetStateAction<boolean>>}) {
     const [tags, setTags] = useState<string[]>([])
     const [statPrivate, setStatPrivate] = useState<boolean>(false)
+    const [serverImg, setServerImg] = useState<File | undefined>()
 
     const inputRef = useRef<HTMLInputElement>(null)
 
     const handleInput = (e: React.KeyboardEvent) => {
     let event = (e.target as HTMLInputElement)
         if (e.key === 'Enter') {
-            if(tags.length <= 20) {
+            if(tags.length <= 20 && !tags.includes(event.value)) {
                 setTags([...tags, event.value])
             }
             if(inputRef.current) {
@@ -25,52 +27,86 @@ function CreateServer({setShowCreateServer} : {setShowCreateServer:React.Dispatc
         setStatPrivate(!statPrivate)
     }
 
-    const handleChip = (tag: string) => {
-        console.log(tag)
+    const removeChip = (tag: string) => {
+        setTags(removeValue(tags, tag))
+    }
+
+    const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("uploading image!", typeof(e.target.files?.[0]))
+        setServerImg(e.target.files?.[0])
     }
 
   return (
     <div className='fixed z-30 w-screen h-screen top-0 left-0 flex justify-center items-center bg-black/40'>
-        <div className="z-40 w-2/5 p-4 h-fit border-4 bg-yellow-400 opacity-100 flex flex-col justify-center items-center gap-4">
-            <div className="flex justify-between border-4 w-full">
+        <div className="z-40 w-2/6 p-4 h-fit bg-yellow-400 rounded-md    opacity-100 flex flex-col justify-center items-center gap-4">
+            <div className="flex justify-between w-4/5">
                 <div className="text-[24px]">Customize your server</div>
                 <div className="hover:cursor-pointer" onClick={() => setShowCreateServer(false)}>
                     <XIcon/>
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center aspect-square rounded-full border-4 w-32">
-                <CameraIcon />
-                <span>upload</span>
+            
+            
+            <div className="relative">
+                <div className="flex flex-col bg-white justify-center items-center aspect-square rounded-full border-2 w-32 hover:cursor-pointer">
+                    {serverImg ? <img src={URL.createObjectURL(serverImg)} className="w-full h-full rounded-full aspect-square" /> : 
+                    <div className="border-4 w-full h-full aspect-square rounded-full flex flex-col justify-center items-center">
+                        <CameraIcon />
+                        <span>upload</span>
+                    </div>}
+                </div>
+                <input 
+                    type="file" 
+                    className="absolute top-0 left-0 bottom-0 right-0 border-4 aspect-square rounded-full w-32 bg-transparent hover:cursor-pointer opacity-0" 
+                    onChange={uploadImage}/>
             </div>
-            <div className="border-4 w-4/5 flex items-center gap-2">
+            <div className="w-4/5 flex items-center gap-2">
                 <label>
-                    <input type="checkbox" checked={statPrivate} onChange={handleCheckChange} className="sr-only peer"/>
+                    <input 
+                        type="checkbox" 
+                        checked={statPrivate} 
+                        onChange={handleCheckChange} 
+                        className="sr-only peer"/>
                     <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-green-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-red-400"></div>
                 </label>
                 {statPrivate ?  <span>Private</span> : <span>Public</span>}
             </div>
-            <div className="border-4 w-4/5">
-                <input type="text" placeholder="server's name" className="w-full" />
+            <div className="w-4/5">
+                <input 
+                    type="text" 
+                    placeholder="server's name" 
+                    className="w-full p-2 rounded-sm text-[18px] mb-2" />
+                <div className="text-red-500">Required field</div>
             </div>
-            <div className="border-4 w-4/5">
-                <textarea className="text-black w-full resize-none" placeholder="description" cols={30} rows={5} />
+            <div className="w-4/5">
+                <textarea 
+                    className="text-black w-full resize-none text-[18px]" 
+                    placeholder="description" 
+                    cols={30} 
+                    rows={5} />
             </div>
-            <div className="border-4 w-4/5 flex flex-col gap-2">
-                <div>Tags</div>
-                <div className="flex flex-wrap border-4">
+            <div className="w-4/5 flex flex-col gap-2">
+                <div className="text-[20px]">Tags</div>
+                <div className="flex flex-wrap max-h-52 overflow-scroll scrollbar-hidden">
                     {tags && tags.map((tag, index) => (
-                        <div key={index} onClick={() => handleChip(tag)}>
+                        <div key={index} onClick={() => removeChip(tag)}>
                             <Chip label={tag} padding="p-1"/>
                         </div>
                     ))}
                 </div>
                 <div>
-                    <input ref={inputRef} type="text" placeholder="Add a tag" onKeyDown={handleInput} />
+                    <input ref={inputRef} 
+                        type="text" 
+                        placeholder="Add a tag" 
+                        onKeyDown={handleInput}
+                        maxLength={20}
+                        className="w-full p-1" />
                 </div>
-                <div>
+                <div className="italic text-[12px]">
                     20 max tags
                 </div>
             </div>
+            <button className="bg-white w-4/5 p-1 rounded-md">Create</button>
         </div>
     </div>
   )
---
+++ frontend/src/features/server/util/Util.ts
@@ -0,0 +1,9 @@
+export const removeValue = (arr: string[], value: string) => {
+    const index = arr.indexOf(value);
+    if (index !== -1) {
+        return [...arr.slice(0, index), ...arr.slice(index + 1)];
+    }
+    return arr;
+}
+
+
