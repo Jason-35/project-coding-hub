581e3a0b4300541d299779459c437cec3c932c26
Jason <jsonwu34@gmail.com>
2025-03-13 13:17:45 -0400

    chore: fixed img import

--- frontend/src/components/UserProfile/UserImg.tsx
+++ frontend/src/components/UserProfile/UserImg.tsx
@@ -1,11 +1,11 @@
-import kitty from "../../../assets/kitty.png"
+import KITTY from "../../assets/kitty.png"
 import { getUserInfo } from "../../features/auth/util/util"
 
 function UserImg() {
     let profileImg = false
     return (
         <div className="rounded-lg aspect-square w-16 flex justify-center items-center bg-orange-500 p-2">
-            {profileImg ? <img src={kitty} alt="" /> : <span className="text-[28px] text-white">{getUserInfo()?.username.charAt(0)}</span>}
+            {profileImg ? <img src={KITTY} alt="" /> : <span className="text-[28px] text-white">{getUserInfo()?.username.charAt(0)}</span>}
         </div>
   )
 }
